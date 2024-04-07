78c59771a003f55093033b4bf41dabc78d2ff374
Enzo Immig <enzoimmig@gmail.com>
2024-04-07 18:08:34 -0300

    Win/Lost page added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 import country_list from './Data/countries_list.json' with { type: 'json' };
 
+const maxTries = 5;
+let curTry = 0;
+
 window.onload = function(){
     initialize();
 }
@@ -8,15 +11,37 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+function winningPage(msg) {
+    let winPage = document.createElement('div')
+    winPage.id = 'win-page'
+    document.getElementById('body').appendChild(winPage)
+
+    let winTitle = document.createElement('h1')
+    winTitle.id = 'win-title'
+    winTitle.innerText = msg
+    winPage.appendChild(winTitle)
+
+    let restartButton = document.createElement('button')
+    restartButton.id = 'restart-button'
+    restartButton.textContent = 'Reiniciar'
+    restartButton.addEventListener('click', function(){
+        location.reload()
+    })
+    winPage.appendChild(restartButton)
+}
+
 function initialize(){
 
     let auxRnd = getRndInteger(0, 240)    
-    let country = document.getElementById("nome_pais")
-
     let codeLC = country_list[auxRnd]['code'].toLowerCase()
     let img = document.createElement('img')
     img.src = `./Data/Flags/${codeLC}.png`
     document.getElementById('body').appendChild(img)
+
+    const attempts = document.createElement('h2')
+    attempts.id = 'num-attempts'
+    document.getElementById('body').appendChild(attempts)
+    attempts.innerText = curTry.toString() + '/' + maxTries.toString()
 
     const guessesList = document.createElement('div')
     guessesList.id = 'guesses-list'
@@ -54,11 +79,17 @@ function initialize(){
                     guessesList.appendChild(guess)
 
                     if(inputField.value == country_list[auxRnd]['name']){
-                        console.log("Acertou!!");
-                        location.reload()
+                        winningPage("You won! Congrats!");
                     }
 
                     inputField.value = '';
+
+                    curTry += 1;
+                    attempts.innerText = curTry.toString() + '/' + maxTries.toString()
+
+                    if(curTry === maxTries){
+                        winningPage("You lost! You got it in the next one!");
+                    }
                 });
                 optionsList.appendChild(optionButton)
             });
@@ -66,11 +97,6 @@ function initialize(){
         }
 
     })
-
-    inputField.addEventListener('focusout', function() {
-        //optionsList.style.display = 'none'
-    })
-
     return true
 }
 
